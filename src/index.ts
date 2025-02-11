52d769bfd82cdead76f41b8954253a6751fe7022
Madi <abulkanov.madi@gmail.com>
2025-02-11 15:15:10 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { AdminRoute } from './routes/admin.route';
 import { ProductRoute } from '@/routes/product.route';
 
 const app = new App({
-  port: 8000,
+  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 8000,
   middlewares: [logger(), cors()],
   routes: [ new AdminRoute(), new ProductRoute() ],
 });
