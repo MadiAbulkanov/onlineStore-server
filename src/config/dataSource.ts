250e1b10484895008052b3e0b064bd70adfb8b24
Madi <abulkanov.madi@gmail.com>
2025-02-11 15:04:37 +0500

    preparation for deployment

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,13 +1,19 @@
 import { DataSource } from 'typeorm';
+import 'dotenv/config';
 
 export const appDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'root',
-  database: 'online-shop',
-  schema: 'online-shop',
+  url: process.env.DATABASE_URL,
+  
+  // username: process.env.USER,
+  // password: process.env.PASSWORD,
+  // database: process.env.PGDATABASE,
+  // host: 'localhost',
+  // port: 5432,
+  // username: 'postgres',
+  // password: 'root',
+  // database: 'online-shop',
+  // schema: 'online-shop',
   synchronize: true,
   logging: true,
   entities: ['src/entities/*{.js,.ts}']
